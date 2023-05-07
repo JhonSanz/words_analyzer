992f027c1e09fe88a16bb2f050d2b90b92f4e94e
JhonSanz <ingjhonsanz@utp.edu.co>
2023-05-07 12:19:58 -0500

    updated ignored words

--- ignored_words.py
+++ ignored_words.py
@@ -1,7 +1,9 @@
 IGNORED = [
     "usted", "ustedes", "multimedia", "omitido", "http", "https", "com", "if", "www",
     "bid", "ask", "int", "double", "stop", "return", "pillamos", "insurance", "diff",
+    "true", "youtu", "youtube", "watch", "point", "append"
 
     "melo", "sisas", "man", "re", "parce", "gonorrea", "bacano", "mk", "hp", "ahi",
-    "pues", "visaje", "chimba", "oe", "pille", "hpt", "hptas", "pirobo"
+    "pues", "visaje", "chimba", "oe", "pille", "hpt", "hptas", "pirobo", "pa", "ale",
+    "mierda", "dizque"
 ]
