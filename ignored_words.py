e721c1ff2d2cea146036a8a7d36c92dab09a4e06
JhonSanz <ingjhonsanz@utp.edu.co>
2023-05-07 12:08:32 -0500

    Added whatsapp library

---
+++ ignored_words.py
@@ -0,0 +1,7 @@
+IGNORED = [
+    "usted", "ustedes", "multimedia", "omitido", "http", "https", "com", "if", "www",
+    "bid", "ask", "int", "double", "stop", "return", "pillamos", "insurance", "diff",
+
+    "melo", "sisas", "man", "re", "parce", "gonorrea", "bacano", "mk", "hp", "ahi",
+    "pues", "visaje", "chimba", "oe", "pille", "hpt", "hptas", "pirobo"
+]
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import numpy as np
+import re
 import pandas as pd
 import nltk
 from unidecode import unidecode
+from whatstk import WhatsAppChat
 from nltk.tokenize import RegexpTokenizer
+from ignored_words import IGNORED
+
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-sw_nltk = stopwords.words('spanish') + ["usted", "ustedes"]
+sw_nltk = stopwords.words('spanish') + IGNORED
 sw_nltk = list(map(lambda x: unidecode(x), sw_nltk))
 
 # TODO: ignorar enlaces (http:// https://)
@@ -15,56 +18,29 @@ sw_nltk = list(map(lambda x: unidecode(x), sw_nltk))
 # TODO: ignorar numeros
 # TODO: ignorar JAJAJAJA jajajajaj hahahaha ejeje uyyy uffff mmmmmmm
 
-data = [
-    {
-        "date": "2020-01-01",
-        "text": """
-            El hijo de rana, Rinrín renacuajo
-            Salió esta mañana muy tieso y muy majo
-            Con pantalón corto, corbata a la moda
-            Sombrero encintado y chupa de boda.
-            -¡Muchacho, no salgas!- le grita mamá
-            pero él hace un gesto y orondo se va.
-        """
-    },
-    {
-        "date": "2020-01-02",
-        "text": """
-            Halló en el camino, a un ratón vecino
-            Y le dijo: -¡amigo!- venga usted conmigo,
-            Visitemos juntos a doña ratona
-            Y habrá francachela y habrá comilona.
-            A poco llegaron, y avanza ratón,
-            Estírase el cuello, coge el aldabón,
-            Da dos o tres golpes, preguntan: ¿quién es?
-            -Yo doña ratona, beso a usted los pies
-            ¿Está usted en casa? -Sí señor sí estoy,
-            y celebro mucho ver a ustedes hoy;
-        """
-    },
-    {
-        "date": "2020-01-03",
-        "text": """
-            estaba en mi oficio, hilando algodón,
-            pero eso no importa; bienvenidos son.
-            Se hicieron la venia, se dieron la mano,
-            Y dice Ratico, que es más veterano:
-            Mi amigo el de verde rabia de calor,
-            Démele cerveza, hágame el favor.
-        """
-    },
-]
-
 class Solver:
     def __init__(self, df):
         self.df = df
         self.words_count = {}
 
+    def check_word(self, word):
+        if (
+            len(word) < 2 or
+            word in sw_nltk or
+            not word.isalpha() or
+            re.search(r'^[jah]*$', word) or
+            re.search(r'^[uy]*$', word) or
+            re.search(r'^[mm]*$', word) or
+            re.search(r'^[uf]*$', word)
+        ):
+            return False
+        return True
+
     def proccess_row(self, paragraph):
         tokenizer = RegexpTokenizer(r'\w+')
         paragraph = unidecode(paragraph.lower())
         paragraph = tokenizer.tokenize(paragraph)
-        paragraph = [word for word in paragraph if word not in sw_nltk]
+        paragraph = [word for word in paragraph if self.check_word(word)]
         return paragraph
 
     def count_words(self, paragraph):
@@ -75,16 +51,16 @@ class Solver:
                 self.words_count[word] = 1
     
     def run(self):
-        for (text,) in zip(self.df["text"]):
-            paragraph = self.proccess_row(text)
+        for (message,) in zip(self.df["message"]):
+            paragraph = self.proccess_row(message)
             self.count_words(paragraph)
         return self.words_count
     
-df = pd.DataFrame(data)
+df = WhatsAppChat.from_source(filepath="chat.txt").df
+
 result = Solver(df).run()
 
 count = pd.DataFrame.from_dict(result, orient='index', columns=['count']).reset_index()
 count.columns = ['word', 'count']
-count = count.sort_values('count')
-print(count)
-
+count = count.sort_values('word').reset_index(drop=True)
+count.to_csv("result.csv")
